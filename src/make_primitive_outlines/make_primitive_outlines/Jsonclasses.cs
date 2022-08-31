e236f244b60b478343caca6d6a6130cc56737d3a
Bert Temme <btemme@hotmail.com>
2022-08-31 21:02:17 +0200

    drawing box with outline correctly

--- src/make_primitive_outlines/make_primitive_outlines/Jsonclasses.cs
+++ src/make_primitive_outlines/make_primitive_outlines/Jsonclasses.cs
@@ -1,25 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace make_primitive_outlines
-{
-}
-
-public class Rootobject
-{
-    public string[] extensionsUsed { get; set; }
-}
-
-
-//public class Rootobject
-//{
-//    public Extensions extensions { get; set; }
-//}
-
-public class Extensions
+﻿public class Extensions
 {
     public CESIUM_Primitive_Outline CESIUM_primitive_outline { get; set; }
 }
--- src/make_primitive_outlines/make_primitive_outlines/Program.cs
+++ src/make_primitive_outlines/make_primitive_outlines/Program.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json.Linq;
 using SharpGLTF.Geometry;
 using SharpGLTF.Materials;
 using SharpGLTF.Scenes;
+using SharpGLTF.Schema2;
 using System.Numerics;
 using VPOSNRM = SharpGLTF.Geometry.VertexTypes.VertexPositionNormal;
 
@@ -14,11 +15,10 @@ var material1 = new MaterialBuilder()
 
 var mesh = new MeshBuilder<VPOSNRM>("mesh");
 mesh.AddCube(material1, Matrix4x4.Identity);
-var pivot1 = new NodeBuilder("Cube1").WithLocalTranslation(new Vector3(-5, 0, 0));
+var pivot1 = new NodeBuilder("Cube1").WithLocalTranslation(new Vector3(0, 0, 0));
 var scene = new SceneBuilder();
 scene.AddRigidMesh(mesh, pivot1);
 var model = scene.ToGltf2();
-
 var originalIndices = model.LogicalMeshes[0].Primitives[0].IndexAccessor.AsIndicesArray().ToArray();
 var INDICES_PER_QUAD = 6;
 var quads = originalIndices.Count() / INDICES_PER_QUAD;
@@ -34,13 +34,13 @@ for (var i = 0; i < quads; i++)
     var a = indices[0];
     var b = indices[1];
     var c = indices[2];
-    var d = indices[4];
+    var d = indices[5];
 
     var quad_edges = new uint[] {
-            a, d,
-            d, b,
+            a, b,
             b, c,
-            c, a
+            a, d,
+            c, d
     };
 
     foreach (var item in quad_edges)
@@ -49,9 +49,22 @@ for (var i = 0; i < quads; i++)
         quadBytes.AddRange(bytes);
     }
 }
-var buffer = model.UseBufferView(quadBytes.ToArray());
 
-var gltf = "test.gltf";
+//var quadBytes1 = new List<byte>();
+//for (var i = 1; i < originalIndices.Length; i++)
+//{
+//    var b1 = BitConverter.GetBytes((UInt16)originalIndices[i - 1]);
+//    var b2 = BitConverter.GetBytes((UInt16)originalIndices[i]);
+//    quadBytes1.AddRange(b1);
+//    quadBytes1.AddRange(b2);
+//}
+
+var buffer = model.UseBufferView(quadBytes.ToArray());
+var accessor = model.CreateAccessor("Animation.Input");
+accessor.SetData(buffer, 0, quadBytes.Count / 2, DimensionType.SCALAR, EncodingType.UNSIGNED_SHORT, false);
+// accessor.UpdateBounds();
+
+var gltf = "test10.gltf";
 model.SaveGLTF(gltf);
 
 var o1 = JObject.Parse(File.ReadAllText(gltf));
@@ -59,7 +72,7 @@ string[] extensionsUsed = new List<string>() { "CESIUM_primitive_outline" }.ToAr
 o1.Add("extensionsUsed", JArray.FromObject(extensionsUsed));
 
 var ext = new Extensions();
-ext.CESIUM_primitive_outline = new CESIUM_Primitive_Outline() { indices = buffer.LogicalIndex };
+ext.CESIUM_primitive_outline = new CESIUM_Primitive_Outline() { indices = accessor.LogicalIndex };
 
 o1["meshes"][0]["primitives"][0]["extensions"] = JObject.FromObject(ext);
 
